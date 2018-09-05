7c7597a1c52bd714fb1d70fe85f439f164340ae7
Maggie Fei <36090098+maggiefei@users.noreply.github.com>
2018-09-05 19:47:44 -0400

    Completed technical challenge for ADI Labs application

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+@app.route('/pokemon/<query>', methods=["GET"])
+def main(query):
+    url = "http://pokeapi.co/api/v2/pokemon/" + query
+    response_dict = requests.get(url).json()
+    try:
+        int(query)
+        return render_template("resultsId.html", api_data=response_dict['name'].capitalize(), search=query)
+    except ValueError:
+        return render_template("resultsName.html", api_data=response_dict, search=query.capitalize())
 
+@app.route("/")
+def hello():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
